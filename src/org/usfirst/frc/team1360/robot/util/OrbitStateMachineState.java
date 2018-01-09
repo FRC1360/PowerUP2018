ab7bf30a156b89e382a3815a10af2c7ef549f960
Nick Mertin <nickmertin@gmail.com>
2018-01-09 11:21:02 -0500

    Simplified state functions by handling exceptions within the state machine

--- src/org/usfirst/frc/team1360/robot/util/OrbitStateMachineState.java
+++ src/org/usfirst/frc/team1360/robot/util/OrbitStateMachineState.java
@@ -1,11 +1,5 @@
 package org.usfirst.frc.team1360.robot.util;
 
 public interface OrbitStateMachineState<T extends OrbitStateMachineState<T>> {
-	void runCore(OrbitStateMachineContext<T> context) throws InterruptedException;
-	default void run(OrbitStateMachineContext<T> context) {
-		try {
-			runCore(context);
-		} catch (InterruptedException e) {
-		}
-	}
+	void run(OrbitStateMachineContext<T> context) throws InterruptedException;
 }

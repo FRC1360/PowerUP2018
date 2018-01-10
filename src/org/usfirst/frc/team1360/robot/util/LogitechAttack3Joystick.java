3366a43f879349802f649bc4aa5cad8391badab8
Michael Geddes <michaelgeddes555@gmail.com>
2018-01-09 20:27:30 -0500

    Updated HumanInput and Attack3Joystick classes to reflect new Joystick

--- src/org/usfirst/frc/team1360/robot/util/LogitechAttack3Joystick.java
+++ src/org/usfirst/frc/team1360/robot/util/LogitechAttack3Joystick.java
@@ -25,57 +25,57 @@ public class LogitechAttack3Joystick extends Joystick {
 	
 	public boolean getTrigger()
 	{
-		return this.getRawButton(0);
+		return this.getRawButton(1);
 	}
 	
 	public boolean getButton2()
 	{
-		return this.getRawButton(1);
+		return this.getRawButton(2);
 	}
 	
 	public boolean getButton3()
 	{
-		return this.getRawButton(2);
+		return this.getRawButton(3);
 	}
 	
 	public boolean getButton4()
 	{
-		return this.getRawButton(3);
+		return this.getRawButton(4);
 	}
 	
 	public boolean getButton5()
 	{
-		return this.getRawButton(4);
+		return this.getRawButton(5);
 	}
 	
 	public boolean getButton6()
 	{
-		return this.getRawButton(5);
+		return this.getRawButton(6);
 	}
 	
 	public boolean getButton7()
 	{
-		return this.getRawButton(6);
+		return this.getRawButton(7);
 	}
 	
 	public boolean getButton8()
 	{
-		return this.getRawButton(7);
+		return this.getRawButton(8);
 	}
 	
 	public boolean getButton9()
 	{
-		return this.getRawButton(8);
+		return this.getRawButton(9);
 	}
 	
 	public boolean getButton10()
 	{
-		return this.getRawButton(9);
+		return this.getRawButton(10);
 	}
 	
 	public boolean getButton11()
 	{
-		return this.getRawButton(10);
+		return this.getRawButton(11);
 	}
 
 }

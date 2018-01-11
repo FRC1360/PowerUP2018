9456255f04da2278dc8d0d8f524be857950ad2c5
Nick Mertin <nickmertin@gmail.com>
2018-01-11 08:28:05 -0500

    Added custom annotation targets

--- src/org/usfirst/frc/team1360/robot/util/SingletonSee.java
+++ src/org/usfirst/frc/team1360/robot/util/SingletonSee.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team1360.robot.util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * Indicates that a singleton request on a type should redirect to another type
  * @author nickmertin
  */
+@Target(ElementType.TYPE)
 public @interface SingletonSee {
 	/**
 	 * The type to redirect to
--- src/org/usfirst/frc/team1360/robot/util/SingletonStatic.java
+++ src/org/usfirst/frc/team1360/robot/util/SingletonStatic.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team1360.robot.util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * Indicates that a singleton request for a type should be resolved by a static method of the type
  * @author nickmertin
  */
+@Target(ElementType.TYPE)
 public @interface SingletonStatic {
 	/**
 	 * The name of the static method

bba064bb1b458f3124ca2dc88a34e43072eddc49
Hotneutral <Hotneutral4@gmail.com>
2018-01-10 21:03:29 -0500

    second fix to intake system
    added state machine

--- src/org/usfirst/frc/team1360/robot/subsystem/Intake.java
+++ src/org/usfirst/frc/team1360/robot/subsystem/Intake.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team1360.robot.subsystem;
 
 import org.usfirst.frc.team1360.robot.IO.RobotOutput;
+import org.usfirst.frc.team1360.robot.util.OrbitStateMachine;
+import org.usfirst.frc.team1360.robot.util.OrbitStateMachineContext;
 import org.usfirst.frc.team1360.robot.util.OrbitStateMachineState;
 
 public class Intake implements IntakeProvider {
@@ -9,6 +11,37 @@ public class Intake implements IntakeProvider {
 	
 	private static enum IntakeState implements OrbitStateMachineState<IntakeState>{
 		
+		INTAKE
+		{
+			@Override
+			public void run(OrbitStateMachineContext<IntakeState> context) throws InterruptedException
+			{
+				RobotOutput.getInstance().setClamp(false);
+		    	RobotOutput.getInstance().setIntake(1);
+			}
+			
+		},
+		CLOSED
+		{
+			@Override
+			public void run(OrbitStateMachineContext<IntakeState> context) throws InterruptedException
+			{
+				RobotOutput.getInstance().setClamp(true);
+		    	RobotOutput.getInstance().setIntake(0);
+			}
+		},
+		IDLE
+		{
+			@Override
+			public void run(OrbitStateMachineContext<IntakeState> context) throws InterruptedException
+			{
+				RobotOutput.getInstance().setClamp(false);
+		    	RobotOutput.getInstance().setIntake(0);
+			}
+		};
+		
+		@Override
+		public abstract void run(OrbitStateMachineContext<IntakeState> context) throws InterruptedException;
 	}
 	
 	public final int IDLE = 2;
@@ -17,28 +50,39 @@ public class Intake implements IntakeProvider {
 	
 	private int intakePosition = 2;
 	
+	private OrbitStateMachine<IntakeState> machine = new OrbitStateMachine<IntakeState>(IntakeState.IDLE);
+	
 	@Override
 	public void setPosition(int position) {
 		// TODO Auto-generated method stub
 		
 	    if (position==INTAKE) {
-	    	robotOutput.setClamp(false);
-	    	robotOutput.setIntake(1);
-	    	intakePosition = INTAKE;
 	    	
+	    	intakePosition = INTAKE;
+	    	try {
+				machine.setState(IntakeState.INTAKE);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	    }
 	    else if(position==CLOSED) 
 	    {
-	    	robotOutput.setIntake(0);
-	    	robotOutput.setClamp(true);
 	    	intakePosition = CLOSED;
+	    	try {
+				machine.setState(IntakeState.CLOSED);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	    }
 	    
 	    else if (position==IDLE)
 	    {
-	    	robotOutput.setIntake(0);
-	    	robotOutput.setClamp(false);
 	    	intakePosition = IDLE;
+	    	try {
+				machine.setState(IntakeState.IDLE);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	    }
 	    
 	}

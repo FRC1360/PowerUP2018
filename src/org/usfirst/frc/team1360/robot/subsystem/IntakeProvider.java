f4c6c6c495d0cf612a8ed45be1edbc5e4e78839a
Nick Mertin <nickmertin@gmail.com>
2018-01-13 18:20:04 -0500

    Fixed merge conflicts

--- src/org/usfirst/frc/team1360/robot/subsystem/IntakeProvider.java
+++ src/org/usfirst/frc/team1360/robot/subsystem/IntakeProvider.java
@@ -4,17 +4,6 @@ import org.usfirst.frc.team1360.robot.util.SingletonType;
 
 @SingletonType(Intake.class)
 public interface IntakeProvider{
-<<<<<<< HEAD
 	void setPosition(double position);
 	int getPosition();	
-=======
-	
-	void setPosition(double position);
-	
-	int getPosition();
-	
-	
-	
-	
->>>>>>> parent of 9d34d53... Removed non eleveator files
 }

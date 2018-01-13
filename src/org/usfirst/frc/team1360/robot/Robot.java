35a4ac5eeaf9f1c7a840a1a27364530b975344b2
Nick Mertin <nickmertin@gmail.com>
2018-01-13 12:39:38 -0500

    Fixed various issues in sigleton implementation

--- src/org/usfirst/frc/team1360/robot/Robot.java
+++ src/org/usfirst/frc/team1360/robot/Robot.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the build.properties file in the
  * project.
  */
+@SingletonStatic
 public class Robot extends TimedRobot {
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";

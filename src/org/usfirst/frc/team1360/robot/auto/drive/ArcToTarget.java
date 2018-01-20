c25d9b383e52a467e92e8f588193372a35dbad0b
Nick Mertin <nickmertin@gmail.com>
2018-01-20 15:35:31 -0500

    Worked on ArcToTarget

--- src/org/usfirst/frc/team1360/robot/auto/drive/ArcToTarget.java
+++ src/org/usfirst/frc/team1360/robot/auto/drive/ArcToTarget.java
@@ -25,11 +25,12 @@ public final class ArcToTarget extends AutonRoutine {
 
 	@Override
 	protected void runCore() throws InterruptedException {
+		log.write(String.format("ArcToTarget (%f, %f | %f) -> (%f, %f)", xs, ys, startAngle, x, y));
 		OrbitPID pid = new OrbitPID(1.0, 0.0, 0.0);
 		double dx = x - xs;
 		double dy = y - ys;
 		double lineAngle = Math.atan2(dx, dy);
-		double da = 2 * (startAngle - lineAngle) % (Math.PI * 2);
+		double da = 2 * (lineAngle - startAngle) % (Math.PI * 2);
 		if (da == 0) { // we're pointing straight at target
 			new MoveToTarget(0, xs, ys, x, y, 0).runUntilFinish();
 			return;
@@ -38,35 +39,37 @@ public final class ArcToTarget extends AutonRoutine {
 			da -= Math.PI * 2; // change angles >180 to negative angles >-180
 		if (da == Math.PI) // we're pointing straight away from target
 			da -= 0.00001;
-		double r = Math.sqrt(dx * dx + dy * dy) / Math.sin(Math.abs(da) / 2);
+		double r = Math.sqrt(dx * dx + dy * dy) / (2 * Math.sin(Math.abs(da) / 2));
 		double r2 = r * r;
 		double angleYSO = startAngle + Math.copySign(Math.PI / 2, da); // angle between +y axis, start, origin
 		double xOrigin = xs + r * Math.sin(angleYSO);
 		double yOrigin = ys + r * Math.cos(angleYSO);
 		double neutral = Math.copySign(Math.log((2 * r + DRIVE_WIDTH) / (2 * r - DRIVE_WIDTH)), da);
+		log.write(String.format("ArcToTarget configured: line=%f da=%f <YSO=%f r=%f origin=(%f, %f)", lineAngle, da, angleYSO, r, xOrigin, yOrigin));
 		double rl2, rl, al;
 		int lLast = sensorInput.getLeftDriveEncoder();
 		int rLast = sensorInput.getRightDriveEncoder();
+		double output = neutral;
 		do {
+			if (output > 0)
+				robotOutput.tankDrive(1.0, Math.exp(-output));
+			else
+				robotOutput.tankDrive(Math.exp(output), 1.0);
 			Thread.sleep(10);
 			double _x = position.getX() - xOrigin;
 			double _y = position.getY() - yOrigin;
 			rl2 = _x * _x + _y * _y;
 			rl = Math.sqrt(rl2);
-			al = Math.atan2(_x, _y) - startAngle + Math.PI / 2;
+			al = (Math.atan2(_x, _y) - startAngle + Math.PI / 2) % (Math.PI * 2);
 			int dl = sensorInput.getLeftDriveEncoder() - lLast;
 			int dr = sensorInput.getRightDriveEncoder() - rLast;
 			if (dl == 0 || dr == 0)
 				continue;
-			double output = neutral + pid.calculate(neutral + Math.copySign((r - rl) / r, da), Math.log((double) dl / dr));
+			output = neutral + pid.calculate(neutral + Math.copySign(Math.log(Math.abs((r - rl) / r)), da), Math.log((double) dl / dr));
 			lLast += dl;
 			rLast += rl;
-			if (output > 0)
-				robotOutput.tankDrive(1.0, Math.exp(-output));
-			else
-				robotOutput.tankDrive(Math.exp(output), 1.0);
-			log.write(String.format("ArcToTarget %f,%f | %f", rl, al * 180 / Math.PI, output));
-		} while (rl2 + r2 - 2 * rl * r * Math.cos(da - al) > epsilon);
+			log.write(String.format("ArcToTarget %d %d | %f,%f | %f", dl, dr, rl, al * 180 / Math.PI, output));
+		} while (al < da && rl2 + r2 - 2 * rl * r * Math.cos(da - al) > epsilon);
 		robotOutput.tankDrive(0, 0);
 	}
 	
--- src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
+++ src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1360.robot.auto.routines;
 
 import org.usfirst.frc.team1360.robot.auto.AutonRoutine;
+import org.usfirst.frc.team1360.robot.auto.drive.ArcToTarget;
 import org.usfirst.frc.team1360.robot.auto.drive.TurnToAngle;
 
 public final class Test extends AutonRoutine {
@@ -11,6 +12,8 @@ public final class Test extends AutonRoutine {
 
 	@Override
 	protected void runCore() throws InterruptedException {
-		new TurnToAngle(0, 90).runUntilFinish();
+//		new TurnToAngle(0, 90).runUntilFinish();
+		position.reset(0, 0, 0);
+		new ArcToTarget(3000, 0, 0, 40, 40, 0, 1).runUntilFinish();
 	}
 }

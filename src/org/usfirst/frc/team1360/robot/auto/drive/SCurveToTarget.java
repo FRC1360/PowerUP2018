c7ad9938a2268f3434d9f4dd6e9ae6b0d725d804
Nick Mertin <nickmertin@gmail.com>
2018-01-25 11:14:24 -0500

    Wrote SCurveToTarget

---
+++ src/org/usfirst/frc/team1360/robot/auto/drive/SCurveToTarget.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team1360.robot.auto.drive;
+
+import org.usfirst.frc.team1360.robot.auto.AutonRoutine;
+
+public final class SCurveToTarget extends AutonRoutine {
+	private double xs;
+	private double ys;
+	private double x;
+	private double y;
+	private double startAngle;
+	private double epsilon;
+
+	public SCurveToTarget(long timeout, double xs, double ys, double x, double y, double startAngle, double epsilon) {
+		super("SCurveToTarget", timeout);
+		this.xs = xs;
+		this.ys = ys;
+		this.x = x;
+		this.y = y;
+		this.startAngle = startAngle * Math.PI / 180;
+		this.epsilon = epsilon * epsilon;
+	}
+
+	@Override
+	protected void runCore() throws InterruptedException {
+		double xCenter = (xs + x) / 2;
+		double yCenter = (ys + y) / 2;
+		double dx = x - xs;
+		double dy = y - ys;
+		double lineAngle = Math.atan2(dx, dy);
+		double startAngle2 = startAngle + 2 * (lineAngle - startAngle);
+		new ArcToTarget(0, x, y, xCenter, yCenter, startAngle, epsilon).runUntilFinish();
+		new ArcToTarget(0, xCenter, yCenter, x, y, startAngle2, epsilon);
+	}
+}

16f26da3132d4c127a2f61cb30a5e303d1b0887d
Nick Mertin <nickmertin@gmail.com>
2018-02-17 12:08:05 -0500

    Fixed issues in elevator control

--- src/org/usfirst/frc/team1360/robot/IO/HumanInput.java
+++ src/org/usfirst/frc/team1360/robot/IO/HumanInput.java
@@ -236,7 +236,7 @@ public class HumanInput implements HumanInputProvider {
 	//returns left joystick of elevator of operator controller
 	public double getElevator()
 	{
-		return -this.operator.getY(Hand.kRight);
+		return deadzone(-operator.getY(Hand.kRight), 0.2);
 	}
 	//returns input after comparing to deadzone
 	public double deadzone(double Input, double deadzone) {

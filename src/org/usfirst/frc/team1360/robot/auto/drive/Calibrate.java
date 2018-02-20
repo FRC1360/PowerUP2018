a2f068e9afbc3ebe022c1cd4b8de6f7f0736678b
EthChil <ethanchilderhose@gmail.com>
2018-02-20 09:46:12 -0500

    Made calibrate safer

--- src/org/usfirst/frc/team1360/robot/auto/drive/Calibrate.java
+++ src/org/usfirst/frc/team1360/robot/auto/drive/Calibrate.java
@@ -1,10 +1,7 @@
 package org.usfirst.frc.team1360.robot.auto.drive;
 
-import org.usfirst.frc.team1360.robot.IO.RobotOutputProvider;
-import org.usfirst.frc.team1360.robot.IO.SensorInput;
 import org.usfirst.frc.team1360.robot.auto.AutonRoutine;
-import org.usfirst.frc.team1360.robot.util.OrbitPID;
-import org.usfirst.frc.team1360.robot.util.Singleton;
+
 
 public class Calibrate extends AutonRoutine{
 	
@@ -15,10 +12,12 @@ public class Calibrate extends AutonRoutine{
 	@Override
 	protected void runCore() throws InterruptedException
 	{
-		arm.safety(-1);
-		Thread.sleep(500);
-		arm.safety(1);
-		while(!sensorInput.getArmSwitch()) arm.safety(1);
+		robotOutput.setArm(-1);
+		while(sensorInput.getArmEncoder() < 500) Thread.sleep(10);
+		robotOutput.setArm(0.75);
+		while(!sensorInput.getArmSwitch()) robotOutput.setArm(0.75);
+		sensorInput.resetArmEncoder();
+		robotOutput.setArm(0);
 		
 		
 	}

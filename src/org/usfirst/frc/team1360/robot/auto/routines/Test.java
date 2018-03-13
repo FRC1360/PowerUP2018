eb8caaada79871daa1800b16e4963e43be349b50
EthChil <ethanchilderhose@gmail.com>
2018-03-13 12:37:42 -0400

    Added jaci tracking

--- src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
+++ src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
@@ -2,11 +2,14 @@ package org.usfirst.frc.team1360.robot.auto.routines;
 
 import org.usfirst.frc.team1360.robot.auto.AutonRoutine;
 import org.usfirst.frc.team1360.robot.auto.drive.DriveToDistance;
-import org.usfirst.frc.team1360.robot.auto.drive.DriveToInch;
-import org.usfirst.frc.team1360.robot.auto.drive.FaceAngle;
 import org.usfirst.frc.team1360.robot.auto.drive.SweepTurn;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.followers.EncoderFollower;
+import jaci.pathfinder.modifiers.TankModifier;
 
 
 
@@ -18,23 +21,39 @@ public final class Test extends AutonRoutine {
 
 	@Override
 	protected void runCore() throws InterruptedException {
-//		sensorInput.reset();
-//		robotOutput.tankDrive(1, -1);
-//		long start = System.currentTimeMillis();
-//		while (sensorInput.getLeftDriveEncoder() - sensorInput.getRightDriveEncoder() == 0) Thread.yield();
-//		long next = System.currentTimeMillis();
-//		SmartDashboard.putNumber("Victor lag", (next - start) / 1000.0);
-//		while (Math.abs(sensorInput.getAHRSYaw()) < 0.25) Thread.yield();
-//		SmartDashboard.putNumber("NavX Lag", (System.currentTimeMillis() - next) / 1000.0);
-//		robotOutput.tankDrive(0, 0);
-		
-//		new SweepTurn(0, 30, true, false).runUntilFinish();
 
-		long start = System.currentTimeMillis();
-		SmartDashboard.putNumber("Auto time", 0);
-		sensorInput.reset();
-		Thread.sleep(10);
-		new FaceAngle(0, 90).runUntilFinish();
-		SmartDashboard.putNumber("Auto time", (System.currentTimeMillis() - start) / 1000.0);
+		Waypoint[] point = new Waypoint[] {
+				new Waypoint(0,0,0),
+				new Waypoint(0,0,0)
+		};
+		
+		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
+		Trajectory trajectory = Pathfinder.generate(point, config);
+		
+		TankModifier modifier = new TankModifier(trajectory).modify(0.627);
+		
+		EncoderFollower left = new EncoderFollower(modifier.getLeftTrajectory());
+		EncoderFollower right = new EncoderFollower(modifier.getRightTrajectory());
+		
+		left.configureEncoder(sensorInput.getLeftDriveEncoder(), 83, 0.127);
+		right.configureEncoder(sensorInput.getRightDriveEncoder(), 83, 0.127);
+		
+		left.configurePIDVA(1.0, 0.0, 0.0, 1 / 1, 0);
+		right.configurePIDVA(1.0, 0.0, 0.0, 1 / 1, 0);
+		
+		double l, r, turn;
+		
+		while(!left.isFinished() || !right.isFinished()) {
+			l = left.calculate(sensorInput.getLeftDriveEncoder());
+			r = right.calculate(sensorInput.getRightDriveEncoder());
+			
+			turn = 0.8 * (-1.0/80.0) * (Pathfinder.d2r(left.getHeading()) - sensorInput.getAHRSYaw());
+			
+			robotOutput.tankDrive(l + turn, r - turn);
+		}
+		
+		robotOutput.setIntake(1.0);
+		Thread.sleep(10000);
+		
 	}
 }

5d533a09fca0e976edaa4d37d1fbbc24dc7ba40a
EthChil <ethanchilderhose@gmail.com>
2018-03-13 13:17:30 -0400

    Profile playing

--- src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
+++ src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
@@ -23,9 +23,14 @@ public final class Test extends AutonRoutine {
 	protected void runCore() throws InterruptedException {
 
 		Waypoint[] point = new Waypoint[] {
-				new Waypoint(0,0,0),
+				new Waypoint(2.5,-1,45),
+				new Waypoint(2,0,0),
+				new Waypoint(1.5,0.5,34),
+				new Waypoint(1,0,0),
 				new Waypoint(0,0,0)
 		};
+		
+		matchLogger.writeClean("PATHFINDER STARTING");
 		
 		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
 		Trajectory trajectory = Pathfinder.generate(point, config);
@@ -38,8 +43,8 @@ public final class Test extends AutonRoutine {
 		left.configureEncoder(sensorInput.getLeftDriveEncoder(), 83, 0.127);
 		right.configureEncoder(sensorInput.getRightDriveEncoder(), 83, 0.127);
 		
-		left.configurePIDVA(1.0, 0.0, 0.0, 1 / 1, 0);
-		right.configurePIDVA(1.0, 0.0, 0.0, 1 / 1, 0);
+		left.configurePIDVA(1.0, 0.0, 0.0, 1 / 3, 0);
+		right.configurePIDVA(1.0, 0.0, 0.0, 1 / 3, 0);
 		
 		double l, r, turn;
 		
@@ -47,11 +52,13 @@ public final class Test extends AutonRoutine {
 			l = left.calculate(sensorInput.getLeftDriveEncoder());
 			r = right.calculate(sensorInput.getRightDriveEncoder());
 			
-			turn = 0.8 * (-1.0/80.0) * (Pathfinder.d2r(left.getHeading()) - sensorInput.getAHRSYaw());
+			turn = -(0.8 * (-1.0/80.0) * (Pathfinder.d2r(left.getHeading()) - sensorInput.getAHRSYaw()));
+			
+			matchLogger.writeClean("PATHFINDER left = " + l + " right = " + r + " turn = " + turn);
 			
 			robotOutput.tankDrive(l + turn, r - turn);
 		}
-		
+		robotOutput.tankDrive(0, 0);
 		robotOutput.setIntake(1.0);
 		Thread.sleep(10000);
 		

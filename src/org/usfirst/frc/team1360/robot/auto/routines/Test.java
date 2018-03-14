4ad5d42bc856039c546ae50c3b6589a24e8667a2
EthChil <ethanchilderhose@gmail.com>
2018-03-14 15:15:18 -0400

    Revert "Added switch path"
    This reverts commit 4d98193ded88e181e419c21f81ba63f0761b2b3f.

--- src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
+++ src/org/usfirst/frc/team1360/robot/auto/routines/Test.java
@@ -22,27 +22,13 @@ public final class Test extends AutonRoutine {
 	@Override
 	protected void runCore() throws InterruptedException {
 
-		/*
+		
 		Waypoint[] points = new Waypoint[] {
 				new Waypoint(0, 0, 0),
 				new Waypoint(4, 4, Pathfinder.d2r(90))
 				//new Waypoint(11, 18, 0)
-			};
-			*/
-		
-		/*
-		Waypoint[] points = new Waypoint[] {
-			new Waypoint(1.63, 4, 0),
-			new Waypoint(16, 2, 0),
-			new Waypoint(19.5, 6, Pathfinder.d2r(90)),
-			new Waypoint(19.5, 17, Pathfinder.d2r(90)),
-			new Waypoint(23, 21, Pathfinder.d2r(-15)),
 		};
-		
-		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 6, 6, 60);
-		Trajectory trajectory = Pathfinder.generate(points, config);
-		*/
-		
+
 		PathfindFromFile path = new PathfindFromFile(10000, "ScaleRacing.csv");
 		path.runNow("To Switch");
 		

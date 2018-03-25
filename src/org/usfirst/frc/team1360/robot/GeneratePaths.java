7134d296e6718455c1c3937da62883b182c84f1a
Michael Geddes <michaelgeddes555@gmail.com>
2018-03-24 20:05:08 -0400

    Removed duplicate and added support for right switch profiles

--- src/org/usfirst/frc/team1360/robot/GeneratePaths.java
+++ src/org/usfirst/frc/team1360/robot/GeneratePaths.java
@@ -17,8 +17,19 @@ public class GeneratePaths {
         Trajectory trajectorySwitchRScaleL1;
         Trajectory trajectorySwitchRScaleL2;
         Trajectory trajectorySwitchLScaleR;
+
         Trajectory trajectorySwitchL;
         Trajectory trajectorySwitchR;
+
+        Trajectory trajectorySwitchL2;
+        Trajectory trajectorySwitchL3;
+        Trajectory trajectorySwitchL4;
+        Trajectory trajectorySwitchL5;
+
+        Trajectory trajectorySwitchR2;
+        Trajectory trajectorySwitchR3;
+        Trajectory trajectorySwitchR4;
+        Trajectory trajectorySwitchR5;
 
         //Two Cube Switch Scale
         //Switch Left Scale Left two cube
@@ -61,7 +72,6 @@ public class GeneratePaths {
                 new Waypoint(14, 21.5, Pathfinder.d2r(270))
         };
 
-
         //SWITCH AUTO PATHS
         Waypoint[] pointsSwitchR = new Waypoint[] {
                 new Waypoint(1.63, 8.5, 0),
@@ -75,6 +85,39 @@ public class GeneratePaths {
         };
 
 
+        Waypoint[] pointsSwitchL2 = new Waypoint[] {
+
+        };
+
+        Waypoint[] pointsSwitchL3 = new Waypoint[] {
+
+        };
+
+        Waypoint[] pointsSwitchL4 = new Waypoint[] {
+
+        };
+
+        Waypoint[] pointsSwitchL5 = new Waypoint[] {
+
+        };
+
+
+        Waypoint[] pointsSwitchR2 = new Waypoint[] {
+
+        };
+
+        Waypoint[] pointsSwitchR3 = new Waypoint[] {
+
+        };
+
+        Waypoint[] pointsSwitchR4 = new Waypoint[] {
+
+        };
+
+        Waypoint[] pointsSwitchR5 = new Waypoint[] {
+
+        };
+
         //CONFIGS
         //Two Cube
         Trajectory.Config configSwitchRScaleR = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 8, 4, 100);
@@ -86,6 +129,16 @@ public class GeneratePaths {
         //Switch Only
         Trajectory.Config configSwitchL = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);
         Trajectory.Config configSwitchR = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+
+        Trajectory.Config configSwitchL2 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+        Trajectory.Config configSwitchL3 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+        Trajectory.Config configSwitchL4 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+        Trajectory.Config configSwitchL5 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+
+        Trajectory.Config configSwitchR2 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+        Trajectory.Config configSwitchR3 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+        Trajectory.Config configSwitchR4 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
+        Trajectory.Config configSwitchR5 = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.025, 7, 4, 100);//jerk was 180
 
 
         //FILES
@@ -99,6 +152,16 @@ public class GeneratePaths {
         //Switch Only
         File fileSwitchL = new File(FILE_ROOT + "switchL.csv");
         File fileSwitchR = new File(FILE_ROOT + "switchR.csv");
+
+        File fileSwitchL2 = new File(FILE_ROOT + "switchL2.csv");
+        File fileSwitchL3 = new File(FILE_ROOT + "switchL3.csv");
+        File fileSwitchL4 = new File(FILE_ROOT + "switchL4.csv");
+        File fileSwitchL5 = new File(FILE_ROOT + "switchL5.csv");
+
+        File fileSwitchR2 = new File(FILE_ROOT + "switchR2.csv");
+        File fileSwitchR3 = new File(FILE_ROOT + "switchR3.csv");
+        File fileSwitchR4 = new File(FILE_ROOT + "switchR4.csv");
+        File fileSwitchR5 = new File(FILE_ROOT + "switchR5.csv");
 
 
 
@@ -114,6 +177,16 @@ public class GeneratePaths {
         trajectorySwitchL = Pathfinder.generate(pointsSwitchL, configSwitchL);
         trajectorySwitchR = Pathfinder.generate(pointsSwitchR, configSwitchR);
 
+        trajectorySwitchL2 = Pathfinder.generate(pointsSwitchL2, configSwitchL2);
+        trajectorySwitchL3 = Pathfinder.generate(pointsSwitchL3, configSwitchL3);
+        trajectorySwitchL4 = Pathfinder.generate(pointsSwitchL4, configSwitchL4);
+        trajectorySwitchL5  = Pathfinder.generate(pointsSwitchL5, configSwitchL5);
+
+        trajectorySwitchR2 = Pathfinder.generate(pointsSwitchR2, configSwitchR2);
+        trajectorySwitchR3 = Pathfinder.generate(pointsSwitchR3, configSwitchR3);
+        trajectorySwitchR4 = Pathfinder.generate(pointsSwitchR4, configSwitchR4);
+        trajectorySwitchR5 = Pathfinder.generate(pointsSwitchR5, configSwitchR5);
+
 
         //SAVE TRAJECTORIES
         //Two Cube Profiles
@@ -126,6 +199,16 @@ public class GeneratePaths {
         //Switch Profiles
         Pathfinder.writeToCSV(fileSwitchL, trajectorySwitchL);
         Pathfinder.writeToCSV(fileSwitchR, trajectorySwitchR);
+
+        Pathfinder.writeToCSV(fileSwitchL2, trajectorySwitchL2);
+        Pathfinder.writeToCSV(fileSwitchL3, trajectorySwitchL3);
+        Pathfinder.writeToCSV(fileSwitchL4, trajectorySwitchL4);
+        Pathfinder.writeToCSV(fileSwitchL5, trajectorySwitchL5);
+
+        Pathfinder.writeToCSV(fileSwitchR2, trajectorySwitchR2);
+        Pathfinder.writeToCSV(fileSwitchR3, trajectorySwitchR3);
+        Pathfinder.writeToCSV(fileSwitchR4, trajectorySwitchR4);
+        Pathfinder.writeToCSV(fileSwitchR5, trajectorySwitchR5);
     }
 
 }

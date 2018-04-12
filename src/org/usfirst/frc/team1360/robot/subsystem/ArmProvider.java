46f28706208d03e676156c805e29b36615c087e5
Ethan Childerhose <ethanchilderhose@gmail.com>
2018-04-12 12:16:16 -0400

    Tuned arm positions

--- src/org/usfirst/frc/team1360/robot/subsystem/ArmProvider.java
+++ src/org/usfirst/frc/team1360/robot/subsystem/ArmProvider.java
@@ -2,8 +2,8 @@ package org.usfirst.frc.team1360.robot.subsystem;
 
 public interface ArmProvider {
 	public static final int POS_BEHIND = 1900;//TUNE
-	public static final int POS_TOP = 2350;
-	public static final int POS_BOTTOM = 3100;//-45
+	public static final int POS_TOP = 2560;
+	public static final int POS_BOTTOM = 3350;//-45
 	
 	void safety(double power, boolean override);
 	boolean idle();

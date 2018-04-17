6633edd77cde89eec2de80843fa86824140a02e5
Ethan Childerhose <ethanchilderhose@gmail.com>
2018-04-16 20:24:54 -0400

    Cleaned path generation further

--- src/org/usfirst/frc/team1360/robot/GeneratePaths.java
+++ src/org/usfirst/frc/team1360/robot/GeneratePaths.java
@@ -13,11 +13,25 @@ public class GeneratePaths {
     public static String fileRoot = "/home/lvuser/";
     public static final double TIME_STEP = 0.025;
 
-    public static Waypoint[] mirrorPoints(Waypoint[] points){
+    public static Waypoint[] mirrorPoint(Waypoint[] points){
         for(int i = 0; i < points.length; i++){
             points[i].y = 27 - points[i].y;
         }
         return points;
+    }
+
+    //Code that will generate a path given input and a specified path
+    public static void generatePathWithMirr(String name, double velocity, double acceleration, double jerk, Waypoint... points){
+        Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, TIME_STEP, velocity, acceleration, jerk);
+
+        File file = new File(fileRoot + name);
+        File fileMir = new File(fileRoot + "M" + name);
+
+        Trajectory trajectoryMir = Pathfinder.generate(mirrorPoint(points), config);
+        Trajectory trajectory = Pathfinder.generate(points, config);
+
+        Pathfinder.writeToCSV(file, trajectory);
+        Pathfinder.writeToCSV(fileMir, trajectoryMir);
     }
 
     //Code that will generate a path given input and a specified path
@@ -117,136 +131,68 @@ public class GeneratePaths {
 
         System.out.println("Starting Switch Path Generation");
 
-        //SWITCH AUTO PATHS
-        Waypoint[] pointsSwitchR = new Waypoint[] {
-                new Waypoint(1.63, 12.5, 0),
-                new Waypoint(9.5, 9.5, Pathfinder.d2r(-30))
-        };
-
-        Waypoint[] pointsSwitchL = new Waypoint[] {
-                new Waypoint(1.63, 12.5, 0),
-                new Waypoint( 5  , 14, Pathfinder.d2r(30)),
-                new Waypoint(9.5, 17.5, Pathfinder.d2r(30)),
-        };
-
-        //Left Second Cube
-        Waypoint[] pointsSwitchL2 = new Waypoint[] {
-                new Waypoint(9.5, 17.5, Pathfinder.d2r(30)),
-                new Waypoint(5.5, 15.5, Pathfinder.d2r(30))
-        };
-
-        Waypoint[] pointsSwitchL3 = new Waypoint[] {
-                new Waypoint(5.5, 15.5, Pathfinder.d2r(-35)),
-                new Waypoint(6.75, 14.7, Pathfinder.d2r(-35))
-        };
-
-        Waypoint[] pointsSwitchL4 = new Waypoint[] {
-                new Waypoint(6.75, 14.75, Pathfinder.d2r(-35)),
-                new Waypoint(6, 15, Pathfinder.d2r(-30))
-        };
-
-        Waypoint[] pointsSwitchL5 = new Waypoint[] {
-                new Waypoint(6, 15, Pathfinder.d2r(30)),
-                new Waypoint(10, 17.5, Pathfinder.d2r(20))
-        };
-
-        //Left Third Cube
-        Waypoint[] pointsSwitchL6 = new Waypoint[] {
-                new Waypoint(10, 17.5, Pathfinder.d2r(20)),
-                new Waypoint(7.5, 16.5, Pathfinder.d2r(25))
-        };
-
-        Waypoint[] pointsSwitchL7 = new Waypoint[] {
-                new Waypoint(7.5, 16.5, Pathfinder.d2r(-45)),
-                new Waypoint(8.5, 15.62, Pathfinder.d2r(-45))
-        };
-
-        Waypoint[] pointsSwitchL8 = new Waypoint[] {
-                new Waypoint(8.5, 15.63, Pathfinder.d2r(-45)),
-                new Waypoint(8, 16, Pathfinder.d2r(-45))
-        };
-
-        Waypoint[] pointsSwitchL9 = new Waypoint[] {
-                new Waypoint(8, 16, Pathfinder.d2r(25)),
-                new Waypoint(10, 17, 20)
-        };
-
-
-        //Right Second Cube
-        Waypoint[] pointsSwitchR2 = new Waypoint[] {
-                new Waypoint(9.5, 9.5, Pathfinder.d2r(-30)),
-                new Waypoint(5.5, 11.5,  Pathfinder.d2r(-30))
-
-        };
-
-        Waypoint[] pointsSwitchR3 = new Waypoint[] {
-                new Waypoint(5.5, 11.5, Pathfinder.d2r(35)),
-                new Waypoint(6.75, 12.25, Pathfinder.d2r(35))
-        };
-
-        Waypoint[] pointsSwitchR4 = new Waypoint[] {
-                new Waypoint(6.75, 12.25, Pathfinder.d2r(35)),
-                new Waypoint(6, 12, Pathfinder.d2r(30))
-        };
-
-        Waypoint[] pointsSwitchR5 = new Waypoint[] {
-                new Waypoint(6, 12, Pathfinder.d2r(-30)),
-                new Waypoint(10, 9.5, Pathfinder.d2r(-20))
-        };
-
-        //Right Third Cube
-        Waypoint[] pointsSwitchR6 = new Waypoint[] {
-                new Waypoint(10, 9.5, Pathfinder.d2r(-20)),
-                new Waypoint(7.5, 10.5, Pathfinder.d2r(-25))
-        };
-
-        Waypoint[] pointsSwitchR7 = new Waypoint[] {
-                new Waypoint(7.5, 10.5, Pathfinder.d2r(45)),
-                new Waypoint(8.5, 11.38, Pathfinder.d2r(45))
-        };
-
-        Waypoint[] pointsSwitchR8 = new Waypoint[] {
-                new Waypoint(8.5, 11.38, Pathfinder.d2r(45)),
-                new Waypoint(8, 11, Pathfinder.d2r(45))
-        };
-
-        Waypoint[] pointsSwitchR9 = new Waypoint[] {
-                 new Waypoint(8, 11, Pathfinder.d2r(-25)),
-                new Waypoint(10, 9.5, Pathfinder.d2r(-20))
-        };
-
-        // DCMP Semifinals RL
-        Waypoint[] pointsDcmpSfRL = new Waypoint[] {
-                new Waypoint(1.63, 4.5, 0),
-                new Waypoint(13, 4.5, 0)
-        };
-
         generatePath("switchL.csv", 8, 10, 75,
                 new Waypoint(1.63, 12.5, 0),
                 new Waypoint( 5  , 14, Pathfinder.d2r(30)),
-                new Waypoint(9.5, 17.5, Pathfinder.d2r(30))
-        );
+                new Waypoint(9.5, 17.5, Pathfinder.d2r(30)));
+        generatePath("switchL2.csv", 8, 10, 75,
+                new Waypoint(9.5, 17.5, Pathfinder.d2r(30)),
+                new Waypoint(5.5, 15.5, Pathfinder.d2r(30)));
+        generatePath("switchL3.csv", 8, 6, 75,
+                new Waypoint(5.5, 15.5, Pathfinder.d2r(-35)),
+                new Waypoint(6.75, 14.7, Pathfinder.d2r(-35)));
+        generatePath("switchL4.csv", 8, 10, 75,
+                new Waypoint(6.75, 14.75, Pathfinder.d2r(-35)),
+                new Waypoint(6, 15, Pathfinder.d2r(-30)));
+        generatePath("switchL5.csv", 8, 10, 75,
+                new Waypoint(6, 15, Pathfinder.d2r(30)),
+                new Waypoint(10, 17.5, Pathfinder.d2r(20)));
+        generatePath("switchL6.csv", 8, 10, 75,
+                new Waypoint(10, 17.5, Pathfinder.d2r(20)),
+                new Waypoint(7.5, 16.5, Pathfinder.d2r(25)));
+        generatePath("switchL7.csv", 8, 6, 75,
+                new Waypoint(7.5, 16.5, Pathfinder.d2r(-45)),
+                new Waypoint(8.5, 15.62, Pathfinder.d2r(-45)));
+        generatePath("switchL8.csv", 8, 10, 75,
+                new Waypoint(8.5, 15.63, Pathfinder.d2r(-45)),
+                new Waypoint(8, 16, Pathfinder.d2r(-45)));
+        generatePath("switchL9.csv", 8, 10, 75,
+                new Waypoint(8, 16, Pathfinder.d2r(25)),
+                new Waypoint(10, 17, Pathfinder.d2r(20)));
 
-        generatePath("switchL2.csv", 8, 10, 75, pointsSwitchL2);
-        generatePath("switchL3.csv", pointsSwitchL3, 8, 6, 75);
-        generatePath("switchL4.csv", pointsSwitchL4, 8, 10, 75);
-        generatePath("switchL5.csv", pointsSwitchL5, 8, 10, 75);
-        generatePath("switchL6.csv", pointsSwitchL6, 8, 10, 75);
-        generatePath("switchL7.csv", pointsSwitchL7, 8, 6, 75);
-        generatePath("switchL8.csv", pointsSwitchL8, 8, 10, 75);
-        generatePath("switchL9.csv", pointsSwitchL9, 8, 10, 75);
 
-        generatePath("switchR.csv", pointsSwitchR, 8, 10, 75);
-        generatePath("switchR2.csv", pointsSwitchR2, 8, 10, 75);
-        generatePath("switchR3.csv", pointsSwitchR3, 8, 6, 75);
-        generatePath("switchR4.csv", pointsSwitchR4, 8, 10, 75);
-        generatePath("switchR5.csv", pointsSwitchR5, 8, 10, 75);
-        generatePath("switchR6.csv", pointsSwitchR6,8 , 10, 75);
-        generatePath("switchR7.csv", pointsSwitchR7, 8, 6, 75);
-        generatePath("switchR8.csv", pointsSwitchR8, 8, 10, 75);
-        generatePath("switchR9.csv", pointsSwitchR9, 8, 10, 75);
+        generatePath("switchR.csv", 8, 10, 75,
+                new Waypoint(1.63, 12.5, 0),
+                new Waypoint(9.5, 9.5, Pathfinder.d2r(-30)));
+        generatePath("switchR2.csv", 8, 10, 75,
+                new Waypoint(9.5, 9.5, Pathfinder.d2r(-30)),
+                new Waypoint(5.5, 11.5,  Pathfinder.d2r(-30)));
+        generatePath("switchR3.csv", 8, 6, 75,
+                new Waypoint(5.5, 11.5, Pathfinder.d2r(35)),
+                new Waypoint(6.75, 12.25, Pathfinder.d2r(35)));
+        generatePath("switchR4.csv", 8, 10, 75,
+                new Waypoint(5.5, 11.5, Pathfinder.d2r(35)),
+                new Waypoint(6.75, 12.25, Pathfinder.d2r(35)));
+        generatePath("switchR5.csv", 8, 10, 75,
+                new Waypoint(6, 12, Pathfinder.d2r(-30)),
+                new Waypoint(10, 9.5, Pathfinder.d2r(-20)));
+        generatePath("switchR6.csv",8 , 10, 75,
+                new Waypoint(10, 9.5, Pathfinder.d2r(-20)),
+                new Waypoint(7.5, 10.5, Pathfinder.d2r(-25)));
+        generatePath("switchR7.csv", 8, 6, 75,
+                new Waypoint(7.5, 10.5, Pathfinder.d2r(45)),
+                new Waypoint(8.5, 11.38, Pathfinder.d2r(45)));
+        generatePath("switchR8.csv", 8, 10, 75,
+                new Waypoint(8.5, 11.38, Pathfinder.d2r(45)),
+                new Waypoint(8, 11, Pathfinder.d2r(45)));
+        generatePath("switchR9.csv", 8, 10, 75,
+                new Waypoint(8, 11, Pathfinder.d2r(-25)),
+                new Waypoint(10, 9.5, Pathfinder.d2r(-20)));
 
-        generatePath("dcmpSfRL.csv", pointsDcmpSfRL, 8,  6, 50);
+
+        generatePath("dcmpSfRL.csv", 8,  6, 50,
+                new Waypoint(1.63, 4.5, 0),
+                new Waypoint(13, 4.5, 0));
     }
 
     public static void generateScalePaths(String FILE_ROOT){
@@ -257,93 +203,46 @@ public class GeneratePaths {
 
         //DOUBLE CUBE SCALE AUTO PATHS
         //Cross
-        Waypoint[] pointsScaleCross1 = new Waypoint[] {
-                new Waypoint(1.63, 4.50, 0),
+        generatePathWithMirr("scaleCrossRight1", 8, 6, 50, new Waypoint(1.63, 4.50, 0),
                 new Waypoint(15, 3, 0),
                 new Waypoint(19.5, 9, Pathfinder.d2r(90)),
                 new Waypoint(19.5, 17.0, Pathfinder.d2r(90)),
+                new Waypoint(24.5, 21, Pathfinder.d2r(-30)));
+        generatePathWithMirr("scaleCrossRight2", 8, 6, 50,
                 new Waypoint(24.5, 21, Pathfinder.d2r(-30)),
-        };
-
-        Waypoint[] pointsScaleCross2 = new Waypoint[] {
-                new Waypoint(24.5, 21, Pathfinder.d2r(-30)),
-                new Waypoint(18, 20, Pathfinder.d2r(20))
-        };
-
-        Waypoint[] pointsScaleCross3 = new Waypoint[] {
+                new Waypoint(18, 20, Pathfinder.d2r(20)));
+        generatePathWithMirr("scaleCrossRight3", 8, 6, 50,
                 new Waypoint(18, 20, Pathfinder.d2r(20)),
-                new Waypoint(24.5, 21, Pathfinder.d2r(-30))
-        };
-
-        Waypoint[] pointsScaleCross4 = new Waypoint[] {
+                new Waypoint(24.5, 21, Pathfinder.d2r(-30)));
+        generatePathWithMirr("scaleCrossRight4", 8, 6, 50,
                 new Waypoint(24.5, 21, Pathfinder.d2r(-30)),
-                new Waypoint(18.25, 18, Pathfinder.d2r(40))
-        };
-
-        Waypoint[] pointsScaleCross5 = new Waypoint[] {
+                new Waypoint(18.25, 18, Pathfinder.d2r(40)));
+        generatePathWithMirr("scaleCrossRight5", 8, 6, 50,
                 new Waypoint(18.25, 18, Pathfinder.d2r(40)),
-                new Waypoint(24.5, 21, Pathfinder.d2r(-30)),
-        };
+                new Waypoint(24.5, 21, Pathfinder.d2r(-30)));
 
-
-        //Same Side
-        Waypoint[] pointsScaleNear1 = new Waypoint[] {
+        //Near
+        generatePathWithMirr("scaleNearRight1", 8, 6, 50,
                 new Waypoint(1.63, 4.5, 0),
                 new Waypoint(13, 3.5, Pathfinder.d2r(0)),
+                new Waypoint(24.25, 6.5, Pathfinder.d2r(30)));
+        generatePathWithMirr("scaleNearRight2", 8, 6, 50,
                 new Waypoint(24.25, 6.5, Pathfinder.d2r(30)),
-        };
-
-
-        Waypoint[] pointsScaleNear2 = new Waypoint[] {
-                new Waypoint(24.25, 6.5, Pathfinder.d2r(30)),
+                new Waypoint(18.75, 6.9, Pathfinder.d2r(-20)));
+        generatePathWithMirr("scaleNearRight3", 8, 6, 50,
                 new Waypoint(18.75, 6.9, Pathfinder.d2r(-20)),
-        };
-
-        Waypoint[] pointsScaleNear3 = new Waypoint[] {
-                new Waypoint(18.75, 6.9, Pathfinder.d2r(-20)),
-                new Waypoint(24.25, 6.5, Pathfinder.d2r(45)),
-        };
-
-        Waypoint[] pointsScaleNear4 = new Waypoint[] {
+                new Waypoint(24.25, 6.5, Pathfinder.d2r(45)));
+        generatePathWithMirr("scaleNearRight4", 8, 6, 50,
                 new Waypoint(24.25, 6.9, Pathfinder.d2r(25)),
+                new Waypoint(18, 8.75, Pathfinder.d2r(-60)));
+        generatePathWithMirr("scaleNearRight5", 8, 6, 50,
                 new Waypoint(18, 8.75, Pathfinder.d2r(-60)),
-        };
-
-        Waypoint[] pointsScaleNear5 = new Waypoint[] {
-                new Waypoint(18, 8.75, Pathfinder.d2r(-60)),
-                new Waypoint(24.25, 6.9, Pathfinder.d2r(25)),
-        };
-
-        generatePath("scaleCrossRight1", pointsScaleCross1, 8, 6, 50);
-        generatePath("scaleCrossRight2", pointsScaleCross2, 8, 6, 50);
-        generatePath("scaleCrossRight3", pointsScaleCross3, 8, 6, 50);
-        generatePath("scaleCrossRight4", pointsScaleCross4, 8, 6, 50);
-        generatePath("scaleCrossRight5", pointsScaleCross5, 8, 6, 50);
-
-        generatePath("scaleNearRight1", pointsScaleNear1, 8, 6, 50);
-        generatePath("scaleNearRight2", pointsScaleNear2, 8, 6, 50);
-        generatePath("scaleNearRight3", pointsScaleNear3, 8, 6, 50);
-        generatePath("scaleNearRight4", pointsScaleNear4, 8, 6, 50);
-        generatePath("scaleNearRight5", pointsScaleNear5, 8, 6, 50);
-
-
-        generatePath("scaleCrossLeft1", mirrorPoints(pointsScaleCross1), 8, 6, 50);
-        generatePath("scaleCrossLeft2", mirrorPoints(pointsScaleCross2), 8, 6, 50);
-        generatePath("scaleCrossLeft3", mirrorPoints(pointsScaleCross3), 8, 6, 50);
-        generatePath("scaleCrossLeft4", mirrorPoints(pointsScaleCross4), 8, 6, 50);
-        generatePath("scaleCrossLeft5", mirrorPoints(pointsScaleCross5), 8, 6, 50);
-
-        generatePath("scaleNearLeft1", mirrorPoints(pointsScaleNear1), 8, 6, 50);
-        generatePath("scaleNearLeft2", mirrorPoints(pointsScaleNear2), 8, 6, 50);
-        generatePath("scaleNearLeft3", mirrorPoints(pointsScaleNear3), 8, 6, 50);
-        generatePath("scaleNearLeft4", mirrorPoints(pointsScaleNear4), 8, 6, 50);
-        generatePath("scaleNearLeft5", mirrorPoints(pointsScaleNear5), 8, 6, 50);
+                new Waypoint(24.25, 6.9, Pathfinder.d2r(25)));
 
         System.out.println("CSVs GENERATED TO PATH " + FILE_ROOT);
     }
 
     public static void main(String [] args){
-
         String FILE_ROOT = "C:\\Users\\orbit\\Desktop\\PowerUP2018\\src\\1360AutoPaths\\";
 
         generateScalePaths(FILE_ROOT);
